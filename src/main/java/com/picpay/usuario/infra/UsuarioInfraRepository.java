5b3d05a88ed3f455e13ce6f2bcf91a9ff1847354
Rauan Santos <rauanmoreira259@gmail.com>
2024-03-25 13:28:33 -0300

    implementa handler e trata exceções das feature de usuario

--- src/main/java/com/picpay/usuario/infra/UsuarioInfraRepository.java
+++ src/main/java/com/picpay/usuario/infra/UsuarioInfraRepository.java
@@ -1,8 +1,10 @@
 package com.picpay.usuario.infra;
 
+import com.picpay.handler.APIException;
 import com.picpay.usuario.dominio.Usuario;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -16,7 +18,11 @@ public class UsuarioInfraRepository implements UsuarioRepository {
     @Override
     public Usuario salva(Usuario usuario) {
         log.info("[inicia] UsuarioInfraRepository - salva");
-        usuarioSpringDataJPA.save(usuario);
+        try {
+            usuarioSpringDataJPA.save(usuario);
+        } catch (DataIntegrityViolationException e){
+            throw APIException.dadosDuplicados("Usuário já cadastrado.");
+        }
         log.info("[finaliza] UsuarioInfraRepository - salva");
         return usuario;
     }
@@ -26,6 +32,7 @@ public class UsuarioInfraRepository implements UsuarioRepository {
         log.info("[inicia] UsuarioInfraRepository - buscaUsuarioPorId");
         Optional<Usuario> usuario = usuarioSpringDataJPA.findById(idUsuario);
         log.info("[finaliza] UsuarioInfraRepository - buscaUsuarioPorId");
-        return usuario.orElseThrow(() -> new RuntimeException("Usuario não encontrado!"));
+        return usuario.orElseThrow(() -> APIException.entidadeNaoEncontrada(
+                String.format("Usuário de id %s não encontrado.", idUsuario)));
     }
 }
