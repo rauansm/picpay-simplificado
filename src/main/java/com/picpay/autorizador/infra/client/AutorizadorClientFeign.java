16f0b919d1b845dc63d15204e238c930ddae8763
Rauan Santos <rauanmoreira259@gmail.com>
2024-04-19 14:47:54 -0300

    configura variavel de ambiente autorizador feign

--- src/main/java/com/picpay/autorizador/infra/client/AutorizadorClientFeign.java
+++ src/main/java/com/picpay/autorizador/infra/client/AutorizadorClientFeign.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "autorizadorClientFeing", url = "https://run.mocky.io/v3/5794d450-d2e2-4412-8131-73d0293ac1cc")
+@FeignClient(name = "autorizadorClientFeing", url = "${autorizador.api.server}")
 public interface AutorizadorClientFeign {
     @GetMapping
   ResponseEntity<AutorizadorResponse> autorizaTransferencia();
